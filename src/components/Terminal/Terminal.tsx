83de4db4b22c6a79c54df1160e82c204f03adf85
jorge20080 <122296192+jorge20080@users.noreply.github.com>
2024-04-17 16:31:06 -0400

    Added more components to use dark mode

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -1,8 +1,10 @@
+import { useDarkModeContext } from '../../context/DarkModeContext';
 import styles from './Terminal.module.css';
 
 const Terminal = () =>{
+    const {isDark} = useDarkModeContext();
     return(
-        <section className={styles.terminal}>
+        <section className={styles.terminal} data-theme={isDark? "dark":undefined}>
             <ul>
                 <li>Problems</li>
                 <li>Output</li>
