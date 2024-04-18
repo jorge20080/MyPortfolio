eee3567d40a88f6cba95cce187a03169bd91686e
jorge20080 <122296192+jorge20080@users.noreply.github.com>
2024-04-18 12:56:35 -0400

    Updated the localstorage util

--- src/context/DarkModeContext.tsx
+++ src/context/DarkModeContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useContext, useState } from "react";
+import { getDarkModeValue, setDarkModeValue } from "../utils/localStorage-helper";
 
 type Props = {
     children:ReactNode
@@ -12,8 +13,10 @@ const darkModeContext = createContext({} as DarkMode)
 export const useDarkModeContext = ()=> useContext(darkModeContext);
 
 const DarkModeProvider = ({children}:Props) =>{
-    const [darkMode, setDarkMode] = useState(false);
+    let storedDarkModeValue = getDarkModeValue() ?? false;
+    const [darkMode, setDarkMode] = useState(storedDarkModeValue);
     const toggleDarkMode = () =>{
+        setDarkModeValue(!darkMode);
         setDarkMode(prev=>!prev);
     }
     return <darkModeContext.Provider value={{isDark: darkMode, toggleDarkMode:toggleDarkMode}}>{children}</darkModeContext.Provider>
--- src/utils/localStorage-helper.ts
+++ src/utils/localStorage-helper.ts
@@ -1,1 +1,10 @@
-//const getDarkMode = ()=> localStorage.getItem('dark-mode');
+export const getDarkModeValue = () :boolean|null => {
+    let value = localStorage.getItem('dark-mode');
+    if(value!==null){
+        return JSON.parse(value);
+    }
+    return null;
+}
+export const setDarkModeValue = (value: boolean) => {
+    localStorage.setItem('dark-mode', JSON.stringify(value));
+}
